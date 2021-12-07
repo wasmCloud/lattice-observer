1a086af019892d90dc51b9d0211134608bcf0cd7
Brooks Townsend <brooks@cosmonic.com>
2021-12-07 11:00:41 -0500

    added provider_start_failed to the events handled
    Signed-off-by: Brooks Townsend <brooks@cosmonic.com>

--- lib/lattice_observer/observed/lattice.ex
+++ lib/lattice_observer/observed/lattice.ex
@@ -184,6 +184,19 @@ defmodule LatticeObserver.Observed.Lattice do
   def apply_event(
         l = %Lattice{},
         %Cloudevents.Format.V_1_0.Event{
+          source: _source_host,
+          datacontenttype: "application/json",
+          time: _stamp,
+          type: "com.wasmcloud.lattice.provider_start_failed"
+        }
+      ) do
+    # This does not currently affect state, but shouldn't generate a warning either
+    l
+  end
+
+  def apply_event(
+        l = %Lattice{},
+        %Cloudevents.Format.V_1_0.Event{
           data:
             %{
               "link_name" => link_name,
