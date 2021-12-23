ab35c0f40f4a4e5db71d28e9c0dcb397327b08df
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2021-12-23 09:55:27 -0500

    Lattice observer now monitors how friendly names
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

--- lib/lattice_observer/observed/host.ex
+++ lib/lattice_observer/observed/host.ex
@@ -2,7 +2,7 @@ defmodule LatticeObserver.Observed.Host do
   alias __MODULE__
 
   @enforce_keys [:id, :labels]
-  defstruct [:id, :labels, :status, :last_seen, :first_seen]
+  defstruct [:id, :labels, :status, :last_seen, :first_seen, :friendly_name]
 
   @typedoc """
   Represents a host observed through heartbeat and start events within a lattice
@@ -12,6 +12,7 @@ defmodule LatticeObserver.Observed.Host do
           labels: Map.t(),
           status: LatticeObserver.Observed.Lattice.entitystatus(),
           last_seen: Datetime.t(),
-          first_seen: Datetime.t()
+          first_seen: Datetime.t(),
+          friendly_name: String.t()
         }
 end
