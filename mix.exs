5fbde00fc6a38d0d4f2216cc28f07c623882fc62
Kevin Hoffman <autodidaddict@users.noreply.github.com>
2023-01-26 14:52:42 -0500

    the power of semver compels me
    Signed-off-by: Kevin Hoffman <autodidaddict@users.noreply.github.com>

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LatticeObserver.MixProject do
   def project do
     [
       app: :lattice_observer,
-      version: "0.2.3",
+      version: "0.3.0",
       elixir: "~> 1.12",
       elixirc_paths: compiler_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
